090330fe86a274006d267a03dc59b635b9a43947
Kris Nóva <kris@nivenly.com>
2021-08-17 15:41:18 -0700

    adding compile unit tests
    Signed-off-by: Kris Nóva <kris@nivenly.com>

---
+++ codify_test.go
@@ -0,0 +1,139 @@
+//
+// Copyright © 2021 Kris Nóva <kris@nivenly.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//   ███╗   ██╗ █████╗ ███╗   ███╗██╗
+//   ████╗  ██║██╔══██╗████╗ ████║██║
+//   ██╔██╗ ██║███████║██╔████╔██║██║
+//   ██║╚██╗██║██╔══██║██║╚██╔╝██║██║
+//   ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗
+//   ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+//
+
+package naml
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+// TestYAMLToGo will generate source code from YAML, and then compile the source, and then run the new program
+func TestYAMLToGo(t *testing.T) {
+	buf := bytes.Buffer{}
+	buf.Write([]byte(TestNginxDeploymentYAML))
+	v := &MainGoValues{
+		AppNameLower:  "testnginx",
+		AppNameTitle:  "TestNginx",
+		AuthorName:    "Björn Nóva",
+		AuthorEmail:   "barnaby@nivenly.com",
+		CopyrightYear: "1999",
+		Description:   "Test nginx deployment.",
+	}
+	code, err := Codify(&buf, v)
+	if err != nil {
+		t.Errorf("Unable to codify test YAML: %v", err)
+	}
+	codeStr := string(code)
+
+	// Check year
+	if !strings.Contains(codeStr, "1999") {
+		t.Errorf("Failure interpolating values into codify.")
+	}
+
+	// Check Install
+	if !strings.Contains(codeStr, "Install") {
+		t.Errorf("Missing Install() in codify.")
+	}
+
+	// Check Uninstall
+	if !strings.Contains(codeStr, "Uninstall") {
+		t.Errorf("Missing Uninstall() in codify.")
+	}
+
+	// Check Objects
+	if !strings.Contains(codeStr, "Objects") {
+		t.Errorf("Missing Objects() in codify.")
+	}
+
+	// Compile the source code
+	program, err := Compile(code)
+	if err != nil {
+		t.Errorf("Unable to compile source code: %v", err)
+		t.FailNow()
+	}
+
+	// Execute the new program
+	stdout, stderr, err := program.Execute([]string{""})
+	if err != nil {
+		t.Errorf("Unable to execute newly compiled program: %v", err)
+	}
+	t.Logf("New Program Stdout Logs: \n%s", stdout.String())
+	t.Logf("New Program Stderr Logs: \n%s", stderr.String())
+
+	// Remove the new program from the filesystem
+	err = program.Remove()
+	if err != nil {
+		t.Errorf("Unable to remove newly compiled program: %v", err)
+	}
+
+}
+
+const (
+	TestNginxDeploymentYAML = `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  annotations:
+    deployment.kubernetes.io/revision: "3"
+    kubectl.kubernetes.io/last-applied-configuration: |
+      {"apiVersion":"apps/appsv1","kind":"Deployment","metadata":{"annotations":{},"name":"nginx-deployment","namespace":"default"},"spec":{"replicas":4,"selector":{"matchLabels":{"app":"nginx"}},"template":{"metadata":{"labels":{"app":"nginx"}},"spec":{"containers":[{"image":"nginx:1.14.2","name":"nginx","ports":[{"containerPort":80}]}]}}}}
+  creationTimestamp: null
+  generation: 3
+  name: nginx-deployment
+  namespace: default
+  resourceVersion: "254882"
+  uid: 445061d9-5000-471b-8e06-45f5240dedb6
+spec:
+  replicas: 4
+  selector:
+    matchLabels:
+      app: nginx
+  strategy:
+    rollingUpdate:
+      maxSurge: 25%
+      maxUnavailable: 25%
+    type: RollingUpdate
+  template:
+    metadata:
+      creationTimestamp: null
+      labels:
+        app: nginx
+    spec:
+      containers:
+      - image: nginx:1.14.2
+        imagePullPolicy: IfNotPresent
+        name: nginx
+        ports:
+        - containerPort: 80
+          protocol: TCP
+        resources: {}
+        terminationMessagePath: /dev/termination-log
+        terminationMessagePolicy: File
+      dnsPolicy: ClusterFirst
+      restartPolicy: Always
+      schedulerName: default-scheduler
+      securityContext: {}
+      terminationGracePeriodSeconds: 30
+`
+)
---
+++ compile.go
@@ -0,0 +1,110 @@
+//
+// Copyright © 2021 Kris Nóva <kris@nivenly.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//   ███╗   ██╗ █████╗ ███╗   ███╗██╗
+//   ████╗  ██║██╔══██╗████╗ ████║██║
+//   ██╔██╗ ██║███████║██╔████╔██║██║
+//   ██║╚██╗██║██╔══██║██║╚██╔╝██║██║
+//   ██║ ╚████║██║  ██║██║ ╚═╝ ██║███████╗
+//   ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝
+//
+
+package naml
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+)
+
+type Source struct {
+	File *os.File
+}
+
+type Program struct {
+	Source *Source
+	File   *os.File
+}
+
+// Execute will execute a compiled NAML program
+func (p *Program) Execute(flags []string) (*bytes.Buffer, *bytes.Buffer, error) {
+	cmd := exec.Command(p.File.Name(), flags...)
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err := cmd.Run()
+	return stdout, stderr, err
+}
+
+// Remove will remove the program from the filesystem
+func (p *Program) Remove() error {
+	return os.Remove(p.File.Name())
+}
+
+// Compile will use the Go compiler to compile source code for NAML
+func Compile(src []byte) (*Program, error) {
+
+	dir := "/tmp"
+
+	// Write a temporary source file
+	f, err := ioutil.TempFile(dir, "*.go")
+	if err != nil {
+		return nil, fmt.Errorf("unable to write temporary file: %v", err)
+	}
+
+	_, err = f.Write(src)
+	defer f.Close()
+	if err != nil {
+		return nil, fmt.Errorf("unable to write source code to temporary file: %v", err)
+	}
+
+	source := &Source{
+		File: f,
+	}
+
+	f, err = ioutil.TempFile(dir, "*.program")
+	if err != nil {
+		return nil, fmt.Errorf("unable to write program file: %v", err)
+	}
+	program := &Program{
+		File: f,
+	}
+
+	fmt.Printf("Source name: %s\n", source.File.Name())
+	fmt.Printf("Program name: %s\n", program.File.Name())
+
+	cmd := exec.Command("go", "build", "-o", program.File.Name(), source.File.Name())
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	cmd.Stdout = stdout
+	cmd.Stderr = stderr
+	err = cmd.Run()
+	if err != nil {
+		e := "\n\n"
+		e = fmt.Sprintf("%s+-------------------------+---------------------------------\n", e)
+		e = fmt.Sprintf("%s| Codify Compile Failure  |\n", e)
+		e = fmt.Sprintf("%s+-------------------------+\n", e)
+		e = fmt.Sprintf("%s| \n", e)
+		e = fmt.Sprintf("%s| %s\n", e, stdout.String())
+		e = fmt.Sprintf("%s| %s\n", e, stderr.String())
+		e = fmt.Sprintf("%s+----------------------------------------------------------\n", e)
+		return nil, fmt.Errorf("%s", e)
+	}
+
+	return program, nil
+}
